3ca4d3f8a91a1a461b367556029b6c20ed06217e
Kevin Park <kevin.park07@yahoo.com>
2020-07-01 18:42:18 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,7 @@
-current_time = Time.authenticate_or_request_with_http_digestcurrent_time.to_i 
-if true 
+current_time = Time.now 
+current_time = current_time.to_i 
+
+if current_time % 2 == 0  
   puts "Even!"
 else
   puts "Odd!"
